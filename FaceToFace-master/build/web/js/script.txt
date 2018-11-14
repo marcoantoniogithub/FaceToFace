c0298c0ded03c36927a322e9a5a901f7921a1ba4
marcoantoniogithub <36649428+marcoantoniogithub@users.noreply.github.com>
2018-11-14 20:28:10 -0200

    Add files via upload

---
+++ FaceToFace-master/build/web/js/script.txt
@@ -0,0 +1,108 @@
+/* 
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+function logar(){
+    inputs = document.getElementsByTagName("input");
+    var porta = false;
+    for (var i=0; i<inputs.length; i++) {
+        if (((inputs[i].type == "text") || (inputs[i].type == "password")) && (inputs[i].value == "")) {
+            alert(inputs[i].name.toUpperCase() + " não pode estar em branco. Favor preencher!");
+            porta = true;
+            break;
+        }
+        else if (((inputs[i].type == "text") || (inputs[i].type == "password")) && inputs[i].value.length < 6){
+            alert(inputs[i].name.toUpperCase() + " deve conter ao menos 6 caracteres!");
+            porta = true;
+            break;
+        }
+        else if (inputs[i].type == "text"){
+            us = i;
+            for(j=0; j<inputs[i].value.length; j++){
+                if(((inputs[i].value.charAt(j) < 65 || inputs[i].value.charAt(j) > 90) && (inputs[i].value.charAt(j) < 97 || inputs[i].value.charAt(j) > 122))){
+                    alert(inputs[i].name.toUpperCase() + " deve conter apenas letras!");
+                    porta = true;
+                    break;
+                }
+            }     
+        }
+        else if (inputs[i].type == "password"){
+            ps = i;
+            for(j=0; j<inputs[i].value.length; j++){
+                 if(((inputs[i].value.charAt(j) < '0' || inputs[i].value.charAt(j) > '9') && (inputs[i].value.charAt(j) < 65 || inputs[i].value.charAt(j) > 90) && (inputs[i].value.charAt(j) < 97 || inputs[i].value.charAt(j) > 122))){
+                    alert(inputs[i].name.toUpperCase() + " deve conter apenas letras ou números!");
+                    porta = true;
+                    break;
+                }
+            }     
+        }
+        else if (inputs[i].type == "checkbox"){
+            if(inputs[i].checked == true){
+                localStorage.setItem("usuario", inputs[us].value);
+                localStorage.setItem("senha", inputs[ps].value);
+            }
+            else{
+                localStorage.removeItem("usuario");
+                localStorage.removeItem("senha");
+            }
+        }
+    }
+    if(porta == true){
+    }
+    else {
+        document.getElementById("id_login").submit();
+    }
+}
+function cadastrar(){
+    inputs = document.getElementsByTagName("input");
+    var porta = false;
+    for (var i=0; i<inputs.length; i++) {
+        if (((inputs[i].type == "text") || (inputs[i].type == "password")) && (inputs[i].value == "")) {
+            alert(inputs[i].name.toUpperCase() + " não pode estar em branco. Favor preencher!");
+            porta = true;
+            break;
+        }
+        else if (((inputs[i].type == "text") || (inputs[i].type == "password")) && inputs[i].value.length < 6){
+            alert(inputs[i].name.toUpperCase() + " deve conter ao menos 6 caracteres!");
+            porta = true;
+            break;
+        }
+        else if (inputs[i].type == "text"){
+            us = i;
+            for(j=0; j<inputs[i].value.length; j++){
+                if(((inputs[i].value.charAt(j) < 65 || inputs[i].value.charAt(j) > 90) && (inputs[i].value.charAt(j) < 97 || inputs[i].value.charAt(j) > 122))){
+                    alert(inputs[i].name.toUpperCase() + " deve conter apenas letras!");
+                    porta = true;
+                    break;
+                }
+            }     
+        }
+        else if (inputs[i].type == "password"){
+            ps = i;
+            for(j=0; j<inputs[i].value.length; j++){
+                 if(((inputs[i].value.charAt(j) < '0' || inputs[i].value.charAt(j) > '9') && (inputs[i].value.charAt(j) < 65 || inputs[i].value.charAt(j) > 90) && (inputs[i].value.charAt(j) < 97 || inputs[i].value.charAt(j) > 122))){
+                    alert(inputs[i].name.toUpperCase() + " deve conter apenas letras ou números!");
+                    porta = true;
+                    break;
+                }
+            }     
+        }
+        else if (inputs[i].type == "checkbox"){
+            if(inputs[i].checked == true){
+                localStorage.setItem("usuario", inputs[us].value);
+                localStorage.setItem("senha", inputs[ps].value);
+            }
+            else{
+                localStorage.removeItem("usuario");
+                localStorage.removeItem("senha");
+            }
+        }
+    }
+    if(porta == true){
+        
+    }
+    else {
+        document.getElementById("id_cadastro").submit();
+    }
+}
---
+++ FaceToFace-master/src/java/Servlet/Logar.java
@@ -0,0 +1,126 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Servlet;
+
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.nio.charset.Charset;
+import java.util.ArrayList;
+import java.util.List;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ *
+ * @author marcov
+ */
+@WebServlet(name = "Logar", urlPatterns = {"/Logar"})
+public class Logar extends HttpServlet {
+
+    /**
+     * Processes requests for both HTTP <code>GET</code> and <code>POST</code>
+     * methods.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    String array = "";
+    String usuario = "";
+    String senha = "";
+    protected void processRequest(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        response.setContentType("text/html;charset=UTF-8");
+        usuario = request.getParameter("n_usuario");
+        senha = request.getParameter("n_senha");
+        List<Jogador> result = le();
+        boolean portaAcesso = false;
+        for(int i =0; i<result.size();i++){
+            System.out.println("pedindo acesso");
+            if((usuario.compareTo(result.get(i).getSenha())==0)&&(senha.compareTo(result.get(i).getNome())==0)){
+                portaAcesso = true;
+            }
+        }
+        if(portaAcesso == false){
+            request.getRequestDispatcher("html/cadastro.html").forward(request, response);
+        }else{
+            request.getRequestDispatcher("html/jogo.html").forward(request, response);
+        }
+    }
+
+    // <editor-fold defaultstate="collapsed" desc="HttpServlet methods. Click on the + sign on the left to edit the code.">
+    /**
+     * Handles the HTTP <code>GET</code> method.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        processRequest(request, response);
+    }
+
+    /**
+     * Handles the HTTP <code>POST</code> method.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        processRequest(request, response);
+    }
+
+    /**
+     * Returns a short description of the servlet.
+     *
+     * @return a String containing servlet description
+     */
+    @Override
+    public String getServletInfo() {
+        return "Short description";
+    }// </editor-fold>
+    public List<Jogador> le() throws FileNotFoundException, IOException
+    {
+            List<Jogador> result = new ArrayList<>();
+            String caminhoDir = getServletContext().getRealPath("/WEB-INF");    
+            File arquivo = new File(caminhoDir,"jogador.txt");
+	    if(arquivo.exists())
+            {
+               String linha;
+               String cli = getServletContext().getRealPath("/WEB-INF/jogador.txt");    
+               BufferedReader b = new BufferedReader(
+                                      new InputStreamReader(
+                                          new FileInputStream(cli), Charset.forName("UTF-8").newDecoder()));
+               while(( linha = b.readLine()) != null)
+                  if ((linha != null) && (!linha.isEmpty()))
+                  {
+                    Jogador c = new Jogador();
+                    String[] vetCliente = linha.split(";");
+                    c.setNome(vetCliente[0]);
+                    c.setUsuario(vetCliente[1]);
+                    c.setSenha(vetCliente[2]);
+                    result.add(c);
+                  }
+               b.close();
+            }
+            return result;
+    }
+}
